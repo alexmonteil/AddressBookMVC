5320ac29544978702741b587766b3d4afad74333
alexmonteil <alex.monteil@outlook.com>
2022-06-21 10:50:41 -0500

    Fixed image override on contact Edit when empty input file

--- AddressBookMVC/Controllers/ContactsController.cs
+++ AddressBookMVC/Controllers/ContactsController.cs
@@ -99,7 +99,7 @@ namespace AddressBookMVC.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address1,Address2,City,State,Zip,Email,Phone,ImageFile")] Contact contact)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Address1,Address2,City,State,Zip,Email,Phone,ImageFile,ImageData,ImageType")] Contact contact)
         {
             if (id != contact.Id)
             {
