7594ffe6ef5029d09dff23939a601a410b4f0b81
alexmonteil <alex.monteil@outlook.com>
2022-06-21 15:46:33 -0500

    style the whole site

--- AddressBookMVC/Controllers/HomeController.cs
+++ AddressBookMVC/Controllers/HomeController.cs
@@ -18,10 +18,11 @@ namespace AddressBookMVC.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult About()
         {
             return View();
         }
+
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
